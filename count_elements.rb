41831c7a1174653c2413c8065e41aca8fdc9e436
Marta Trojanowska <m.troyanovska@gmail.com>
2023-04-23 18:42:23 +0200

    commit

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,17 @@
 class CountElements
+  attr_accessor :array
 
+  def initialize(array)
+    @array = array
+  end
+
+  def perform
+    hash = {}
+    @array.each do |el|
+      hash[el] = @array.count(el)
+    end
+    hash
+  end
 end
+
+CountElements.new([0, 4, 2, 1, 0, 2, 5, 5, 5]).perform
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,44 @@
 class ValidatePassword
+  attr_accessor :password
 
+  def initialize(password)
+    @password = password.chars
+  end
+
+  UPPERCASES = ('A'..'Z').to_a
+  DOWNCASES = ('a'..'z').to_a
+  NUMBERS = ('0'..'9').to_a
+  SPECIALS = '! @ # $ % & * + = : ; ? < >'.chars
+
+  def letter_repetition
+    @password.join =~ /([[:alpha:]])\1{2,}/
+  end
+
+  def password_length
+    return false if letter_repetition
+
+    password.size >= 6 && password.size <= 24
+  end
+
+  def check_upcase
+    (UPPERCASES & password).any?
+  end
+
+  def check_downcase
+    (DOWNCASES & password).any?
+  end
+
+  def check_numbers
+    (NUMBERS & password).any?
+  end
+
+  def check_specials
+    (SPECIALS & password).any?
+  end
+
+  def perform
+    password_length && check_upcase && check_downcase && check_specials && check_numbers
+  end
 end
+
+p ValidatePassword.new('PassswordDD9!!!').perform
